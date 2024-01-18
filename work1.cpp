9c1323cbcb9b5c5137feaaa0b8b640a37bf88cab
Vibhuti <vibhuti6378@gmail.com>
2024-01-18 23:25:35 +0530

    changes made

---
+++ work1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Name: Harshal Wasnik"<<endl;;
+    cout << "Roll No: 21115114"<<endl;;
+    string str;
+    cout << "Enter the string"<<endl;;
+    cin >> str;
+    cout << "The text after XOR operation " << str << endl;
+    return 0;
+}
