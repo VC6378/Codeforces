8b38f4172d2411ba484712dae76daaa8a948d842
Vibhuti <vibhuti6378@gmail.com>
2024-01-27 13:33:07 +0530

    questions

---
+++ A_Square.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t-->0){
+        vector<pair<int,int>> coord(4);
+        for(int i=0;i<4;i++){
+            
+        }
+    }
+    fastio()
+    return 0;
+}
