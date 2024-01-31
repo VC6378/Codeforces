435ee54d04d362c39ad655b6ca375bfa8f4b78e7
Vibhuti <vibhuti6378@gmail.com>
2024-01-31 18:02:06 +0530

    question

---
+++ B_I_Hate_1111.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    ll x;
+    cin>>x;
+    bool flag=false;
+    for(int i=0;i<20;i++){
+        if(x%11==0){
+            flag=true;
+            break;
+        }
+        x-=111;
+        if(x<0) break;
+    }
+    if(flag) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t-- > 0)
+    {
+        solve();
+    }
+    return 0;
+}
