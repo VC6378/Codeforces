855ab7e9d68801c8eeb29ea3208b96f41742e221
Vibhuti <vibhuti6378@gmail.com>
2025-01-09 21:12:51 +0530

    work

--- C_Non_coprime_Split.cpp
+++ C_Non_coprime_Split.cpp
@@ -21,7 +21,7 @@ double eps = 1e-12;
 #define mp make_pair
 #define pb push_back
 #define fi first
-#define se second
+#define se secondc
 #define INF 2e18
 #define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define all(x) (x).begin(), (x).end()
