fe34a5bbb5a35ca13793570bae36ead60b7a45a9
Vibhuti <vibhuti6378@gmail.com>
2025-04-06 21:25:31 +0530

    work

--- B_Pushing_Balls.cpp
+++ B_Pushing_Balls.cpp
@@ -1,68 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll t, n, m, a[59][59], vis[59][59];
-char s[59];
-inline ll read()
+
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+int32_t main()
 {
-    ll s = 0, w = 1;
-    char ch = getchar();
-    while (ch > '9' || ch < '0')
-    {
-        if (ch == '-')
-            w = -1;
-        ch = getchar();
-    }
-    while (ch <= '9' && ch >= '0')
-        s = (s << 1) + (s << 3) + (ch ^ 48), ch = getchar();
-    return s * w;
-}
-int main()
-{
-    t = read();
-    while (t--)
-    {
-        n = read(), m = read();
-        for (ll i = 1; i <= n; i += 1)
-        {
-            scanf("%s", s + 1);
-            for (ll j = 1; j <= m; j += 1)
-            {
-                a[i][j] = s[j] - '0';
-                vis[i][j] = 0;
+    int t;
+    cin>>t;
+    while(t-->0){
+        int n,m;
+        cin>>n>>m;
+        vector<string> a(n);
+        for(int i=0;i<n;i++){
+            string s(m,'0');
+            for(int j=0;j<m;j++){
+                cin>>s[j];
+            }
+            a[i]=s;
+        }
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(a[i][j]=='0') break;
+                vis[i][j]=1;
             }
         }
-        for (ll i = 1; i <= n; i += 1)
-        {
-            for (ll j = 1; j <= m; j += 1)
-            {
-                if (!a[i][j])
-                    break;
-                vis[i][j] = 1;
+
+        for(int j=0;j<m;j++){
+            for(int i=0;i<n;i++){
+                if(a[i][j]=='0') break;
+                vis[i][j]=1;
             }
         }
-        for (ll j = 1; j <= m; j += 1)
-        {
-            for (ll i = 1; i <= n; i += 1)
-            {
-                if (!a[i][j])
-                    break;
-                vis[i][j] = 1;
-            }
-        }
-        bool fl = 1;
-        for (ll i = 1; i <= n && fl; i += 1)
-        {
-            for (ll j = 1; j <= m; j += 1)
-            {
-                if (a[i][j] && !vis[i][j])
-                {
-                    fl = 0;
+
+        bool flag = true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(a[i][j]=='1' && !vis[i][j]){
+                    flag = false;
                     break;
                 }
             }
         }
-        puts(fl ? "YES" : "NO");
+
+        if(flag) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
     }
+
+    fastio()
     return 0;
 }
